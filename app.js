224e87e8392eaf82cb451ba992e4b53ff1caa384
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-02-16 21:38:31 -0800

    Handling Error Routes

--- app.js
+++ app.js
@@ -13,4 +13,18 @@ app.use(morgan('dev'));
 app.use('/products', ProductsApi);
 app.use('/orders', OrdersApi);
 
+app.use((req, res, next)=>{
+    const error = new Error('Not Found');
+    error.status = 404 ;
+    next(error); 
+})
+app.use((req, res, next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    })
+})
+
 module.exports = app;
