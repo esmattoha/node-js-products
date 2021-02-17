c35bdcb88ed91d639bb9e41d3e8966a45e12b852
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-02-16 20:11:46 -0800

    Handle Request

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const ProductsApi = require('./api/routers/products');
 const OrdersApi = require('./api/routers/orders');
 
 const app = express();
 
+
 app.use(morgan('dev'));
 
+// Routes which handle request 
 app.use('/products', ProductsApi);
 app.use('/orders', OrdersApi);
 
