9575a3578ec34c142ababf07f10723394820e059
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-02-18 14:54:24 -0800

    product save in database

--- api/models/product.js
+++ api/models/product.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const productSchema = new Schema({
-    _id: mongoose.Schema.Types.ObjectId(),
+    _id: mongoose.Schema.Types.ObjectId,
     name:{
         type:'String',
     required: true
--- api/routers/products.js
+++ api/routers/products.js
@@ -1,46 +1,57 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('../models/product');
 
 const router = express.Router();
 
 // Handle Get Request for Products
-router.get('/',(req, res, next)=>{
+router.get('/', (req, res, next) => {
     res.status(200).json({
-        message:'All Products are here!'
+        message: 'All Products are here!'
     })
 });
 
 // Handle Post Request for Products
-router.post('/',(req, res, next)=>{
-    const product = {
+router.post('/', (req, res, next) => {
+    const product = new Product({
+        _id:new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    }
-    res.status(201).json({
-        message:'Post operation of Product!',
-        product: product
-    })
+    });
+    product.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Post operation of Product!',
+                product: product
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
 });
 
 // Handle Get Request for Product
-router.get('/:productId',(req, res, next)=>{
+router.get('/:productId', (req, res, next) => {
     res.status(200).json({
-        message:'Fetch a Paticuler Product!',
+        message: 'Fetch a Paticuler Product!',
         id: req.params.productId
     })
 });
 
 // Handle Patch Request for Product
-router.patch('/:productId',(req, res, next)=>{
+router.patch('/:productId', (req, res, next) => {
     res.status(200).json({
-        message:'patch a Paticuler Product!',
+        message: 'patch a Paticuler Product!',
         id: req.params.productId
     })
 });
 
 // Handle Delete Request for Products
-router.delete('/:productId',(req, res, next)=>{
+router.delete('/:productId', (req, res, next) => {
     res.status(200).json({
-        message:'delete a Paticuler Product!',
+        message: 'delete a Paticuler Product!',
         id: req.params.productId
     })
 });
