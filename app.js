56fa713d6cf1660a67cc09ee5e3f2a45eaaed524
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-02-18 14:09:57 -0800

    Access Control Allow

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const ProductsApi = require('./api/routers/products');
 const OrdersApi = require('./api/routers/orders');
@@ -11,6 +12,16 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if(req.method === 'OPTION'){
+        res.header("Access-Control-Allow-Methods",'PUT, PATCH, POST, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 // Routes which handle request 
 app.use('/products', ProductsApi);
