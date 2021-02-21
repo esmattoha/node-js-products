8059c183cc4cac52b3715ac9989ddd214c810762
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-02-21 15:43:47 -0800

    file upload staticly

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/Shoping', {
 });
 
 app.use(morgan('dev'));
+app.use('/uploads',express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
