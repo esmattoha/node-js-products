7f3766ae2a5d0cafa84274e602cb54448c9000c2
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-02-21 15:35:48 -0800

    file mime type

--- api/routers/products.js
+++ api/routers/products.js
@@ -11,9 +11,24 @@ const storage = multer.diskStorage({
     filename: (req, file, cb) => {
         cb(null, new Date().toISOString() + file.originalname);
     }
-})
+});
 
-const upload = multer({storage: storage});
+const fileFilter = (req, file, cb) =>{
+    if(file.mimetype === "image/jpeg" || file.mimetype === "image/png"){
+        cb(null , true);
+    }else{
+        cb(null , false);
+    }
+
+}
+
+const upload = multer({
+    storage: storage, 
+    limits:{
+        fileSize: 1024 * 1024 * 5 
+    },
+    fileFilter: fileFilter
+});
 
 
 const router = express.Router();
