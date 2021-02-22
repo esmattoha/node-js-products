b514b7e7cce9d014e6347dd824b140325f8c548a
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-02-21 22:45:54 -0800

    user model , signup, delete

---
+++ api/routers/user.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/signup', (req, res, next) => {
+    bcrypt.hash(req.body.password, 10, function (err, hash) {
+        if (err) {
+            return res.status(500).json({
+                error: err
+            })
+        } else {
+            const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: req.body.email,
+                password: hash
+            });
+            user.save()
+                .then(result => {
+                    console.log(result);
+                    res.status(201).json({
+                        message: 'User Created!'
+                    })
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        error: err
+                    })
+                })
+        }
+    });
+
+});
+
+router.delete('/:userId', (req, res, next)=>{
+    User.remove({_id : req.params.userId})
+    .exec()
+    .then(result =>{
+        res.status(200).json({
+            message:"User Deleted!"
+        })
+    })
+    .catch(err =>{
+        res.status(500).json({
+            error:err
+        })
+    })
+});
+
+
+
+module.exports = router;
